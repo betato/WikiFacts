cb4ff4d2fdfdc392b368876c72786db5f772ed5a
Betato <BattlestarGatticus@gmail.com>
2016-03-31 16:46:08 -0400

    Created startup argument handler

--- WikiFacts/Program.cs
+++ WikiFacts/Program.cs
@@ -13,10 +13,59 @@ namespace WikiFacts
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            if (args.Length > 0)
+            {
+                // Format argument
+                string arg1 = args[0].ToLower().Trim().Replace('/', '-');
+
+                switch (arg1)
+                {
+                    case "-s":
+                        // Fullscreen mode
+                        run();
+                        break;
+
+                    case "-c":
+                        // Configuration mode
+                        config();
+                        break;
+
+                    case "-p":
+                        // Preview mode
+                        preview();
+                        break;
+
+                    default:
+                        // Unknown argument
+                        run();
+                        break;
+                }
+            }
+            else
+            {
+                // No arguments
+                config();
+            }
+        }
+
+        public static void run()
+        {
+            
+        }
+
+        public static void preview()
+        { 
+
+        }
+
+        public static void config()
+        {
+            // Open configuration window
             Application.Run(new Form1());
         }
     }
